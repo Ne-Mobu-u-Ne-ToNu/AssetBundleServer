73fc8dc24804bf775a5b90c425730592e9a926ef
Ne-Mobu-u-Ne-ToNu <Seriy.sery2014@gmail.com>
2025-03-27 04:23:23 +0300

    add common file template

--- src/main/java/com/usachevsergey/AssetBundleServer/controllers/PageController.java
+++ src/main/java/com/usachevsergey/AssetBundleServer/controllers/PageController.java
@@ -1,26 +1,38 @@
 package com.usachevsergey.AssetBundleServer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    private String templateHtml = "fragments/common";
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(Model model) {
+        generatePage("Главная", "index", model);
+        return templateHtml;
     }
     @GetMapping("/uploadFile")
-    public String files() {
-        return "uploadFile";
+    public String files(Model model) {
+        generatePage("Загрузка файла", "uploadFile", model);
+        return templateHtml;
     }
 
     @GetMapping("/authorization")
-    public String authorization() {
-        return "authorization";
+    public String authorization(Model model) {
+        generatePage("Авторизация", "authorization", model);
+        return templateHtml;
     }
 
     @GetMapping("/registration")
-    public String registration() {
-        return "registration";
+    public String registration(Model model) {
+        generatePage("Регистрация", "registration", model);
+        return templateHtml;
+    }
+
+    private void generatePage(String title, String pageHtmlName, Model model) {
+        model.addAttribute("pageTitle", title);
+        model.addAttribute("fragmentPage", pageHtmlName);
     }
 }
--- src/main/resources/static/js/loadFooter.js
+++ src/main/resources/static/js/loadFooter.js
@@ -1,16 +1,6 @@
-function loadFooter() {
-    fetch('/partials/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-placeholder').innerHTML = data;
-            updateYear(); // Вызываем функцию для обновления года
-        })
-        .catch(error => console.error('Ошибка при загрузке футера:', error));
-}
-
 function updateYear() {
     const currentYear = new Date().getFullYear();
     document.getElementById('current-year').textContent = currentYear;
 }
 
-loadFooter();
+updateYear();
